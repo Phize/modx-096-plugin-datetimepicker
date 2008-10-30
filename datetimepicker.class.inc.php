116120a8047b16b547a5af84bc41b88ac791597c
Phize <info@phize.net>
2008-10-30 14:17:18 +0000

    git-svn-id: svn://localhost/MODx/assets/plugins/datepicker/trunk@23 6ce02de6-cfb7-2e46-b63e-5161d7ce3690

--- datetimepicker.class.inc.php
+++ datetimepicker.class.inc.php
@@ -4,8 +4,8 @@
  * Author     : Phize
  * Author URI : http://phize.net
  * License    : GNU General Public License(http://www.gnu.org/licenses/gpl.html)
- * Version    : 1.0.0
- * Last Update: 2008-10-27
+ * Version    : 1.1.0 beta
+ * Last Update: 2008-10-30
  */
 
 class datetimepicker {
@@ -63,23 +63,17 @@ class datetimepicker {
         $e = &$modx->Event;
 
         if ($this->language == 'en') {
-            $js_i18n = '';
+            $script = <<<EOD
+<script src="{$this->base_uri}js/ui.datetimepicker.js" type="text/javascript" charset="utf-8"></script>
+EOD;
         } else {
-            $js_i18n = <<<EOD
+            $script = <<<EOD
+<script src="{$this->base_uri}js/ui.datetimepicker.js" type="text/javascript" charset="utf-8"></script>
 <script src="{$this->base_uri}js/i18n/ui.datetimepicker-{$this->language}.js" type="text/javascript" charset="utf-8"></script>
 EOD;
         }
 
-        $html = <<<EOD
-<!-- DateTimepicker plugin: BEGIN -->
-<script src="{$this->base_uri}js/jquery.js" type="text/javascript" charset="utf-8"></script>
-<script src="{$this->base_uri}js/ui.datetimepicker.js" type="text/javascript" charset="utf-8"></script>
-{$js_i18n}
-<script type="text/javascript"  charset="utf-8">
-    var modxPluginDateTimepicker = {};
-    modxPluginDateTimepicker.jQuery = jQuery.noConflict(true);
-
-    modxPluginDateTimepicker.jQuery(document).ready(function() {
+        $code = <<<EOD
         // Show datetimepicker
         function showDateTimePicker(event) {
             modxPluginDateTimepicker.jQuery(this).datetimepicker('dialog', modxPluginDateTimepicker.jQuery(event.data.field).val(), function(date) { modxPluginDateTimepicker.jQuery(event.data.field).val(date); }, {
@@ -92,15 +86,35 @@ EOD;
             return false; 
         }
 
+        // Bind datetimepicker event
+        for (var i = 0; i < dates.length; i ++) {
+            modxPluginDateTimepicker.jQuery('.plugin-datetimepicker-trigger-' + (i + 1)).bind('click', { field: '.plugin-datetimepicker-field-' + (i + 1) }, showDateTimePicker);
+        }
+EOD;
+
+        $html = <<<EOD
+<!-- DateTimepicker plugin: BEGIN -->
+<script src="{$this->base_uri}js/jquery.js" type="text/javascript" charset="utf-8"></script>
+{$script}
+<script type="text/javascript"  charset="utf-8">
+    // Avoid conflict with other script
+    var modxPluginDateTimepicker = {};
+    modxPluginDateTimepicker.jQuery = jQuery.noConflict(true);
+
+    modxPluginDateTimepicker.jQuery(document).ready(function() {
         // The array of the selectors to replace datetimepicker
-        var dates = [ { field: 'input[@name="pub_date"]', icon: 'input[@name="pub_date"] ~ a[@onclick]:has(img[src*="cal.gif"])' },
-                     { field: 'input[@name="unpub_date"]', icon: 'input[@name="unpub_date"] ~ a[@onclick]:has(img[src*="cal.gif"])' } ];
+        var dates = [ { field: 'input[@name="pub_date"]', trigger: 'input[@name="pub_date"] ~ a[@onclick]:has(img[src*="cal.gif"])' },
+                     { field: 'input[@name="unpub_date"]', trigger: 'input[@name="unpub_date"] ~ a[@onclick]:has(img[src*="cal.gif"])' } ];
+
+        // Set class name and remove onclick attribute
+        for (var i = 0; i < dates.length; i ++) {
+            modxPluginDateTimepicker.jQuery(dates[i].field).attr({ class: 'plugin-datetimepicker-field-' + (i + 1) });
+            modxPluginDateTimepicker.jQuery(dates[i].trigger).attr({ class: 'plugin-datetimepicker-trigger-' + (i + 1)});
+            modxPluginDateTimepicker.jQuery(dates[i].trigger).removeAttr('onclick');
+        }
 
         // Replace datetimepickers
-        for (var i = 0; i < dates.length; i ++) {
-            modxPluginDateTimepicker.jQuery(dates[i].icon).bind('click', { field: dates[i].field }, showDateTimePicker);
-            modxPluginDateTimepicker.jQuery(dates[i].icon).removeAttr('onclick');
-        }
+{$code}
 
         // Append CSS file
         modxPluginDateTimepicker.jQuery('head').append('<link href="{$this->base_uri}css/jquery.datetimepicker.css" rel="stylesheet" type="text/css" />');
--- readme.ja.txt
+++ readme.ja.txt
@@ -3,8 +3,8 @@ Category   : Plugin
 Author     : Phize
 Author URI : http://phize.net
 License    : GNU General Public License(http://www.gnu.org/licenses/gpl.html)
-Version    : 1.0.0
-Last Update: 2008-10-27
+Version    : 1.1.0 beta
+Last Update: 2008-10-30
 
 
 
